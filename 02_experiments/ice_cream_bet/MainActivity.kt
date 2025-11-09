1cf89bf689c46976a0a9b7c59854b53a191e11bc
todays_sun_day <178suna@gmail.com>
2025-11-09 17:47:22 +0900

    chore: 레포지터리 구조 재구성
    아이스크림 내기 프로젝트는 MainActivity.kt와 AndroidMainfest.xml 제외하고 삭제함.
    WidgetBuddy 프로젝트 추가함.
    강의 폴더 위치 변경함.

---
+++ 02_experiments/ice_cream_bet/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.open8.practice3
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import java.util.*
+import kotlin.concurrent.timer
+import kotlin.math.abs
+
+class MainActivity : AppCompatActivity() {
+    fun main() {
+        setContentView(R.layout.activity_main)
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        var timerTask: Timer? = null
+        var stage = 1
+        var sec = 0f
+        val tvR: TextView = findViewById(R.id.tv_random)
+        val tvT: TextView = findViewById(R.id.tv_timer)
+        val tvP: TextView = findViewById(R.id.tv_point)
+        val btn: Button = findViewById(R.id.btn_kor)
+
+        val randomBox = Random()
+        val randomNum = randomBox.nextInt(1001)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        main()
+
+
+        tvR.text = (randomNum.toFloat() / 100).toString()
+
+
+        btn.setOnClickListener {
+            isRunning = !isRunning
+
+            if (isRunning) {
+                timerTask = timer(period = 10) {
+                    sec++
+                    runOnUiThread {
+                        tvT.text = (sec / 100).toString()
+                    }
+                }
+            } else {
+                timerTask?.cancel()
+                val point = abs(sec - randomNum) / 100
+                tvP.text = point.toString()
+            }
+        }
+    }
+}
