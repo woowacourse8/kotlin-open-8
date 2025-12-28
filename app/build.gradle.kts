65b6f497b74a1978d0c576ada5693f13f8f7b9ee
todays_sun_day <178suna@gmail.com>
2025-12-28 15:26:25 +0900

    feat: 뱁새와 드래곤 이미지 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,15 +12,16 @@ android {
         applicationId = "com.starterkim.widgetbuddy"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
